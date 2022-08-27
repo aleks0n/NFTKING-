8c6e43dbf5daf98501a8a898b9a22292a2d4164d
aleks0n <95637855+aleks0n@users.noreply.github.com>
2022-08-27 21:59:18 +0200

    Add website

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
